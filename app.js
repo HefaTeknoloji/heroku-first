7427d639eed955b9b6da809536f90bf8d6e9da1c
HefaTeknoloji <84381608+HefaTeknoloji@users.noreply.github.com>
2022-09-01 17:17:43 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+ var bodyParser = require("body-parser");
+ var app = express();
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
+ app.post("/", function(req, res) {
+    let total = parseInt(req.body.num2) + parseInt(req.body.num1);
+    console.log('total: '+total.toString());
+     res.send(req.body);
+
+ });
+ app.listen(3000, function () {
+     console.log('Example app listening on port 3000!');
+ });
