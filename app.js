ce34f897ba085006ffa6af6220132e0206d11b1c
HefaTeknoloji <84381608+HefaTeknoloji@users.noreply.github.com>
2022-09-01 17:47:13 +0300

    node engines

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express');
     let total = parseInt(req.body.num2) + parseInt(req.body.num1);
     console.log('total: '+total.toString());
      res.send(req.body);
-
+    
  });
  app.listen(3000, function () {
      console.log('Example app listening on port 3000!');
